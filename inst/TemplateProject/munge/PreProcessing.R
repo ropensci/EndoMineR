e8efae522b08b83ad94205fa57e7155cde18cb27
sebastiz <sebastiz@hotmail.com>
2019-05-06 15:42:01 +0000

    Corrected the new Extractor output to make sure it outputted a dataframe

--- inst/TemplateProject/munge/PreProcessing.R
+++ inst/TemplateProject/munge/PreProcessing.R
@@ -13,12 +13,12 @@
 #Prepare the text for OGD
 mywordsOGD<-c("hospital number:","patient name:","general practitioner","date of procedure:","endoscopist:","2nd endoscopist:","medications:","instrument:","extent of exam:","indications:", "procedure performed:","findings:","diagnosis:")
 #MyOGD<-textPrep(TheOGDReportFinal$OGDReportWhole,mywordsOGD)
-MyOGD<-textPrep(TheOGDReportFinal$OGDReportWhole,mywordsOGD,NegEx="TRUE",Extractor="1")
+MyOGD<-textPrep(TheOGDReportFinal$OGDReportWhole,mywordsOGD,NegEx="TRUE")
 
 #Prepare the text for Pathology
 mywordsPath<-c("hospital number:","patient name:","dob:","general practitioner:","date received:","nature of specimen:","macroscopic description:" ,"diagnosis:")
 #MyPath<-textPrep(PathDataFrameFinal$PathReportWhole,mywordsPath)
-MyPath<-textPrep(PathDataFrameFinal$PathReportWhole,mywordsPath,NegEx="TRUE",Extractor="1")
+MyPath<-textPrep(PathDataFrameFinal$PathReportWhole,mywordsPath,NegEx="TRUE")
 
 
 
