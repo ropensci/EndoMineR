8ba81dd5875abf58911df170f0ae9355a3a79f89
sebastiz <sebastiz@hotmail.com>
2019-06-04 16:48:15 +0000

    Added collapsible panels for endoscopy and pathology data.

--- EndoMineR_Shiny/ui.R
+++ EndoMineR_Shiny/ui.R
@@ -13,102 +13,66 @@ library(shinydashboard)
 library(shinydashboardPlus)
 
 
-dashboardPagePlus(
-  dashboardHeaderPlus(title = 'EndoMineR',
-                      fixed = TRUE,
-                      enable_rightsidebar = TRUE,
-                      dropdownMenu(
-                        type = "tasks",
-                        badgeStatus = "danger",
-                        taskItem(value = 20, color = "aqua", "Refactor code"),
-                        taskItem(value = 40, color = "green", "Design new layout"),
-                        taskItem(value = 60, color = "yellow", "Another task"),
-                        taskItem(value = 80, color = "red", "Write documentation")
-                      )),
-  dashboardSidebar(box(
-    width = NULL,
-    title = "Links and Tutorials",
-    status = NULL,
-    appButton(
-      url = "https://ropensci.github.io/EndoMineR/articles/EndoMineRPrinciples.html",
-      label = "Tutorials for under the hood", 
-      icon = "fa fa-users", 
-      enable_badge = TRUE, 
-      badgeColor = "purple", 
-      badgeLabel = 891
-    ),
-    appButton(
-      label = "Gastro data science", 
-      icon = "fas fa-info-circle", 
-      enable_badge = FALSE, 
-      badgeColor = NULL, 
-      badgeLabel = NULL
-    ),
-    appButton(
-      label = "Likes", 
-      icon = "fa fa-heart-o", 
-      enable_badge = TRUE, 
-      badgeColor = "red", 
-      badgeLabel = 3
-    ),
-    socialButton(
-      url = "https://twitter.com/GastroDS",
-      type = "twitter"
-    ),
-    socialButton(
-      url = "http://github.com/sebastiz",
-      type = "github"
-    )
-  )
-  ),
+dashboardPage(
+  
+  dashboardHeader(title = 'EndoMineR'),
+ 
+  dashboardSidebar(
+    
+    sidebarMenu(
+        menuItem("Dashboard"),
+        fileInput("endoscopy",
+                  label="Upload Endoscopy Dataset here",
+                  multiple = FALSE),
+        fileInput("pathology",
+                  label="Upload Pathology Dataset here",
+                  multiple = FALSE),
+        fileInput("merged",
+                  label="Upload Merged Dataset here",
+                  multiple = FALSE)
+        )),
+
+
+ 
   dashboardBody(
-  
-
-    
-    
-  tabsetPanel(type = "tabs",
-                        
-  tabPanel("Endoscopy Dataset", verbatimTextOutput("summary"),
-           textInput("caption", "Caption", "Data Summary"),
-           actionButton("textPrep",label = "textPrep"),
-           actionButton("EndoscEndoscopist",label = "EndoscEndoscopist"),
-           actionButton("EndoscMeds",label = "EndoscMeds"),
-           actionButton("EndoscInstrument",label = "EndoscInstrument"),
-           DT::dataTableOutput("endotable")
-  ),
-  
-  tabPanel("Pathology Dataset", tableOutput("table"),
-           textInput("captionpath", "Caption", "Data Summary"),
-           actionButton("textPrepPath",label = "textPrepPath"),
-           actionButton("NumBx",label = "NumBx"),
-           actionButton("BxSize",label = "BxSize"),
-           actionButton("EndoscInstrument",label = "EndoscInstrument"),
-           DT::dataTableOutput("pathTable")
-  ),
-  tabPanel("Merged Dataset", tableOutput("table2"),
-           actionButton("Endomerge2",label = "Endomerge2"),
-           actionButton("MetricByEndoscopist",label = "MetricByEndoscopist"),
-           actionButton("CategoricalByEndoscopist",label = "CategoricalByEndoscopist"),
-           actionButton("HowManyOverTime",label = "HowManyOverTime"),
-           DT::dataTableOutput("mergedTable"),
-           verbatimTextOutput('foo'),
-           DT::dataTableOutput("metricTable"),
-           plotOutput("plot")
-  ),
-  tabPanel("Barrett's", tableOutput("table5"),
-           actionButton("PragueScore",label = "PragueScore"),
-           actionButton("PathStage",label = "PathStage"),
-           actionButton("FollowUpType",label = "FollowUpType"),
-           actionButton("AllTheFunctions",label = "AllTheFunctions"),
-           actionButton("SurveillanceTime",label = "SurveillanceTime"),
-           actionButton("SurveillanceLastTest",label = "SurveillanceLastTest"),
-           actionButton("SurveillanceFirstTest",label = "SurveillanceFirstTest")
-  ),
-  tabPanel("Polyps", tableOutput("table3"),
-           actionButton("GRS",label = "GRS"),
-           DT::dataTableOutput("polypTable")
-  )
-  )
-
+    tabsetPanel(type = "tabs",
+                tabPanel("Clean your Dataset", verbatimTextOutput("summary"),
+                                   
+    bsCollapse(id = "collapseExample", open = "Panel 1",
+               bsCollapsePanel("Endoscopy Data", "Upload data to get the endoscopy dataset", style = "info",
+                               textInput("caption", "Caption", "Data Summary"),
+                               actionButton("textPrep",label = "textPrep"),
+                               actionButton("EndoscEndoscopist",label = "EndoscEndoscopist"),
+                               actionButton("EndoscMeds",label = "EndoscMeds"),
+                               actionButton("EndoscInstrument",label = "EndoscInstrument"),
+                               DT::dataTableOutput("endotable")),
+               bsCollapsePanel("Pathology Data", "Upload data to get the pathology dataset", style = "info",
+                               textInput("caption", "Caption", "Data Summary"),
+                               actionButton("textPrepPath",label = "textPrepPath"),
+                               actionButton("NumBx",label = "NumBx"),
+                               actionButton("BxSize",label = "BxSize"),
+                               DT::dataTableOutput("pathTable")),
+               bsCollapsePanel("Merged Data", "Upload data to get the Merged dataset", style = "info",
+                               textInput("caption", "Caption", "Data Summary"),
+                               actionButton("Endomerge2",label = "Endomerge2"),
+                               actionButton("MergeWithImages",label = "MergeWithImages"),
+                               actionButton("CategoricalByEndoscopist",label = "CategoricalByEndoscopist"),
+                               actionButton("HowManyOverTime",label = "HowManyOverTime"),
+                               DT::dataTableOutput("mergedTable")))
+                ),
+                tabPanel("Barrett's", tableOutput("table5"),
+                               actionButton("PragueScore",label = "PragueScore"),
+                               actionButton("PathStage",label = "PathStage"),
+                               actionButton("FollowUpType",label = "FollowUpType"),
+                               actionButton("AllTheFunctions",label = "AllTheFunctions"),
+                               actionButton("SurveillanceTime",label = "SurveillanceTime"),
+                               actionButton("SurveillanceLastTest",label = "SurveillanceLastTest"),
+                               actionButton("SurveillanceFirstTest",label = "SurveillanceFirstTest")
+                    ),
+                tabPanel("Polyps", tableOutput("table3"),
+                               actionButton("GRS",label = "GRS"),
+                               DT::dataTableOutput("polypTable")
+                    )
+    )
   )
 )
