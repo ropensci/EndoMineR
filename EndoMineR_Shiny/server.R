2e847113005ae5b8de5de9c3d9019bb0d8d45c1a
sebastiz <sebastiz@hotmail.com>
2019-07-11 06:59:58 +0000

    Development of plots for Barretts

--- EndoMineR_Shiny/server.R
+++ EndoMineR_Shiny/server.R
@@ -22,6 +22,8 @@ data_r <-reactiveValues(data = data.frame())
 RV2 <- reactiveValues(data = data.frame())
 RV3 <- reactiveValues(data = data.frame())
 RV4 <- reactiveValues(data = data.frame())
+Trim <- reactiveValues(data = data.frame())
+pivotData<-reactiveValues(data = data.frame())
 RV5 <- reactiveValues(data = data.frame())
 RV6 <- reactiveValues(data = data.frame())
 RV7 <- reactiveValues(data = data.frame())
@@ -31,9 +33,9 @@ RV10 <- reactiveValues(data = data.frame())
 
 
 
-server <- function(input, output) {
+server <- function(input, output,session) {
   
-
+#Radiant things
   
  
   observe({
@@ -57,7 +59,7 @@ server <- function(input, output) {
   })
   
   observe({
-    inFile_merged <- input$merged
+    inFile_merged <- input$inFile_merged
     if (!is.null(inFile_merged)) {   
       dataFile <- read_excel(inFile_merged$datapath, sheet=1)
       dat <- data.frame(EndoPaste(dataFile)[1], stringsAsFactors=FALSE)
@@ -82,13 +84,12 @@ server <- function(input, output) {
     pageLength = 5,
     dom = 'Bfrtip',
     buttons = c('copy', 'csv', 'excel', 'pdf', 'print','colvis')))
+
   
  
   
   
   output$BarrettsTable = DT::renderDT({
-    #RV4$data
-    
     RV4$data
       
 
@@ -103,6 +104,8 @@ server <- function(input, output) {
   
   output$mergedTable = DT::renderDT({
     if (!is.null(RV3$data)) {  
+      
+
       
       RV3$data[["Select"]]<-paste0('<input type="checkbox" name="row_selected" value="Row',1:nrow(RV3$data),'"><br>')
     
@@ -161,6 +164,19 @@ server <- function(input, output) {
                                                        dom = 'Bfrtip',
                                                        buttons = c('copy', 'csv', 'excel', 'pdf', 'print','colvis')))
   
+  
+  #Trimmed from the mergedTable data sets:
+  output$trimTable = DT::renderDT({
+   
+    Trim$data<-RV3$data[input$mergedTable_rows_all, input$mergedTable_columns_selected]
+  },selection = list(target = 'column'),extensions = 'Buttons', 
+  options = list(
+    fixedHeader=TRUE,
+    scrollX = TRUE,
+    scrollY = TRUE,
+    pageLength = 5,
+    dom = 'Bfrtip',
+    buttons = c('copy', 'csv', 'excel', 'pdf', 'print','colvis')))
 
 
   output$polypTable = DT::renderDT({
@@ -172,8 +188,9 @@ server <- function(input, output) {
   
   
   output$table1 <- renderDT({
-    #browser()
+    if(!is.null(RV4$data)){
     RV6$data<-data.frame(psych::describe(RV4$data))
+    }
   })
   
   
@@ -185,7 +202,6 @@ server <- function(input, output) {
   observeEvent(input$textPrep,{
     mywordsOGD<-input$caption
     mywordsOGD<-unlist(strsplit(mywordsOGD,","))
-    #browser()
     RV$data<-textPrep(RV$data[,1],mywordsOGD)
    
   },ignoreInit = TRUE)
@@ -215,7 +231,6 @@ server <- function(input, output) {
   
   #Standardise the date
   observeEvent(input$DateStandardiserEPath,{
-    #browser()
     RV2$data[,as.numeric(input$pathTable_columns_selected)]<-parse_date_time(str_extract(RV2$data[,as.numeric(input$pathTable_columns_selected)],
                                                                         "(\\d{4}[[:punct:]]\\d{2}[^:alnum:]\\d{2})|(\\d{2}[^:alnum:]\\d{2}[^:alnum:]\\d{4})"),
                                                                         orders = c("dmy", "ymd"))
@@ -225,9 +240,8 @@ server <- function(input, output) {
   
   
    
-  #Standardise the Hospital NumberEndoscopy
+  #Standardise the Hospital Number Endoscopy
   observeEvent(input$HospitalNumberExtractorEndo,{
-    #browser()
     RV$data[,as.numeric(input$endotable_columns_selected)]<-str_extract(RV$data[,as.numeric(input$endotable_columns_selected)],
                                                                                          "([a-z0-9]\\d{4,}[a-z0-9])")
     
@@ -245,7 +259,6 @@ server <- function(input, output) {
   #Standardise the AlphaNumeric as numeric in Endoscopy
   observeEvent(input$CategoricalDataEndo,{
     RV$data[,as.numeric(input$endotable_columns_selected)]<-as.factor(RV$data[,as.numeric(input$endotable_columns_selected)])
-    
   },ignoreInit = TRUE)
   
   
@@ -254,21 +267,19 @@ server <- function(input, output) {
   },ignoreInit = TRUE)
   
   observeEvent(input$Radiant,{
-radiant.data::launch()  
+    register(RV$data,org = "", descr = "", env)
     },ignoreInit = TRUE)
   
   
   
   #Standardise the Hospital NumberPathology
   observeEvent(input$HospitalNumberExtractorPath,{
-    #browser()
     RV2$data[,as.numeric(input$pathTable_columns_selected)]<-str_extract(RV2$data[,as.numeric(input$pathTable_columns_selected)],
                                                                                          "([a-z0-9]\\d{4,}[a-z0-9])")
   },ignoreInit = TRUE)
   
   #Standardise the Categorical as categorical in Pathology
   observeEvent(input$CategoricalDataPath,{
-    #browser()
     RV$data[,as.numeric(input$pathTable_columns_selected)]<-as.factor(RV$data[,as.numeric(input$pathTable_columns_selected)])
     
   },ignoreInit = TRUE)
@@ -290,7 +301,6 @@ radiant.data::launch()
   
   
   observeEvent(input$MergeImages,{
-    #browser()
     input$captionDelim
     file_selected<-parseFilePaths(volumes, input$Btn_GetFile)
     folder_selected<-parseDirPath(volumes, input$folder)
@@ -304,15 +314,10 @@ radiant.data::launch()
     colnames(Imgdf)[which(names(Imgdf) == "Endo_ResultEntered")] <- "Date"
     Imgdf$Date <- gsub("\n", "", Imgdf$Date)
     Imgdf$Date <- as.Date(Imgdf$Date)
-    
-
-    #colnames(RV3$data)[which(names(RV3$data) == colnames(input$mergedTable_columns_selected[1]))] <- "Date"
-    
    
     names(RV3$data)[as.numeric(input$mergedTable_columns_selected[1])]<- "HospitalNum"
     names(RV3$data)[as.numeric(input$mergedTable_columns_selected[2])]<- "Date"
     RV3$data$Date<-as.Date(RV3$data$Date)
-
     #No need for fuzzy join here as images are from the endoscopy- may need to change this with other images though
     RV3$data<-left_join(RV3$data,Imgdf,by = c("Date","HospitalNum"), copy = FALSE)
     
@@ -381,62 +386,123 @@ radiant.data::launch()
     RV2$data$BxSize<-HistolBxSize(RV2$data$macroscopicdescription)
   },ignoreInit = TRUE)
   
+  
+  
+  
+  
+  ############ Basic Stats Events ############
+  
+  
+  # Return the requested dataset ----
+  datasetInputBasicStats <- reactive({
+    switch(input$datasetBasicStats,
+           "Endoscopy" = RV$data,
+           "Pathology" = RV2$data,
+           "Merged" = RV3$data,
+           "Trimmed" = Trim$data,
+           "Barretts" = RV4$data)
+  })
+  
+
+  
+  
+  
+
+  
+  sumdiplay = reactive({
+    plouf <- summary(datasetInputBasicStats())
+    info <- lapply(colnames(plouf),function(coln){
+      valueBox(
+        coln,
+        paste0(plouf[,coln],collapse = "\n,"),
+        icon = icon("credit-card"),
+        width = 3
+      )
+    })
+    return(info)
+  })
+  
+  
+  output$ibox <- renderUI({
+    sumdiplay()
+  })
+  
+
+  output$OverallPivot <- renderRpivotTable({
+     rpivotTable(Trim$data)
+    })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   ########### EndoMerge events ############  
   
   observeEvent(input$Endomerge2,{
+    
     #Merge the patientID column and date from each table. Make sure that the patient ID is chosen first;
+    
+   
+    
     #Need to fix this to understand when it is selecting the number. I think the user needs to 
+    
     #convert to date and then select columns (date first) at one sitting with the datatable
     
-    # EndoDate<-colnames(RV$data[as.numeric(input$endotable_columns_selected[1])])
-    # EndoNum<-colnames(RV$data[as.numeric(input$endotable_columns_selected[2])])
-    # PathDate<-colnames(RV2$data[as.numeric(input$pathTable_columns_selected[1])])
-    # PathNum<-colnames(RV2$data[as.numeric(input$pathTable_columns_selected[2])])
-    browser()
+    EndoDate<-colnames(RV$data[as.numeric(input$endotable_columns_selected[1])])
     
+    EndoNum<-colnames(RV$data[as.numeric(input$endotable_columns_selected[2])])
     
-    colnames(RV$data[as.numeric(input$endotable_columns_selected[1])])<-"Date"
-    colnames(RV$data[as.numeric(input$endotable_columns_selected[2])])<-"HospitalID"
-    colnames(RV2$data[as.numeric(input$endotable_columns_selected[1])])<-"Date"
-    colnames(RV2$data[as.numeric(input$endotable_columns_selected[2])])<-"HospitalID"
+    PathDate<-colnames(RV2$data[as.numeric(input$pathTable_columns_selected[1])])
     
-    colnames(RV$data)[which(names(RV$data) == colnames(RV$data[as.numeric(input$endotable_columns_selected[1])]))] <- "HospitalID"
-    colnames(RV$data)[which(names(RV$data) == colnames(RV$data[as.numeric(input$endotable_columns_selected[2])]))] <- "Date"
-    
-    colnames(RV2$data)[which(names(RV2$data) == colnames(RV2$data[as.numeric(input$pathTable_columns_selected[1])]))] <- "HospitalID"
-    colnames(RV2$data)[which(names(RV2$data) == colnames(RV2$data[as.numeric(input$pathTable_columns_selected[2])]))] <- "Date"
-    
-    
-    RV3$data<-RV2$data %>% left_join(RV$data, by=setNames(nm=c(EndoNum,EndoDate),c(PathNum,PathDate)))
-    
-    #Get the number of days by subtracting the date columns:
+    PathNum<-colnames(RV2$data[as.numeric(input$pathTable_columns_selected[2])])
     
     
     
+    RV3$data<-Endomerge2(RV$data,colnames(RV$data[as.numeric(input$endotable_columns_selected[1])]),
+                         colnames(RV$data[as.numeric(input$endotable_columns_selected[2])]),
+                         RV2$data,
+                         colnames(RV2$data[as.numeric(input$pathTable_columns_selected[1])]),
+                         colnames(RV2$data[as.numeric(input$pathTable_columns_selected[2])]))
     
+    RV3$data<-RV3$data[,1:ncol(RV3$data)-1]
     
+                         
     
     #Create a copy that can be independently edited for the Barrett's table
-    RV4$data<-RV3$data %>% filter_all(any_vars(str_detect(., pattern = "[Bb]arrett")))
+    #browser()
     
-    # difference_left_join(RV2$data,
-    #                     RV$data,
-    #                    by=setNames(nm=c(EndoNum,EndoDate),c(PathNum,PathDate)),
-    #                   max_dist = 8,
-    #                  distance_col = "Days"
-    # )
-    
-    sooo<-fuzzyjoin::difference_left_join(RV2$data, RV$data, by = 'HospitalID', max_dist = 8, distance_col = 'Days') 
-    
-    #%>%filter(colnames(RV$data[as.numeric(input$pathTable_columns_selected[2])]) == colnames(RV2$data[as.numeric(input$pathTable_columns_selected[2])]))
-    
-    EndoHistoMerge$pHospitalNum <- trimws(EndoHistoMerge$pHospitalNum)
-    EndoHistoMerge$eHospitalNum <- trimws(gsub("\n", "", EndoHistoMerge$eHospitalNum))
-    EndoHistoMerge1 <- EndoHistoMerge[EndoHistoMerge$eHospitalNum == EndoHistoMerge$pHospitalNum, ]
+    #Search for columnar lined or Barrett's terms as per the UMLS
+    #RV4$data<-RV3$data %>% filter_all(any_vars(. %in% c('barrett')))
+    # mtcars %>% filter_all(all_vars(grepl("3", .)))
+    RV4$data <- RV3$data[Reduce(`|`, lapply(RV3$data, grepl, pattern = "columnar.*?lined.*?\\.|barrett")),]
     
     
-    #No need for fuzzy join here as images are from the endoscopy- may need to change this with other images though
+    #Create a copy that can be filtered for the Trim table
+    Trim$data<-RV3$data
+    
   },ignoreInit = TRUE)
+  
+  
+  
+  
+  
+  
 
   
   
@@ -451,15 +517,21 @@ radiant.data::launch()
      RV4$data$CStage<-as.numeric(RV4$data$CStage)
   },ignoreInit = TRUE)
   
+  
+  
   observeEvent(input$PathStage,{
     cols <- as.numeric(input$BarrettsTable_columns_selected)
     selectedCol<-colnames(RV4$data)[cols]
     RV4$data$IMorNoIM<-Barretts_PathStage(RV4$data, selectedCol[1])
   },ignoreInit = TRUE)
   
+  
+  
   observeEvent(input$FollowUpType,{
     RV4$data$FU_Type<-Barretts_FUType(RV4$data, "CStage", "MStage", "IMorNoIM")
   },ignoreInit = TRUE)
+  
+  
   
   observeEvent(input$SurveillanceTime,{
     cols <- as.numeric(input$BarrettsTable_columns_selected)
@@ -469,6 +541,67 @@ radiant.data::launch()
   
   
   
+  output$endoscopistCol<-renderUI({
+    selectInput("endoscopistColChooser", label = h4("Choose the column showing the endoscopist:"),
+                choices = colnames(RV4$data) ,selected = 1
+    )
+  })
+  
+  output$endoscopistPick<-renderUI({
+    selectInput("endoscopistPickChooser", label = h4("Choose the endoscopist:"),
+                choices = unique(RV4$data[,input$endoscopistColChooser]),selected = 1
+    )
+  })
+  
+  output$worstGradeCol<-renderUI({
+    selectInput("WorstGradeChooser", label = h4("Choose the column showing the worst grade"),
+                choices = colnames(RV4$data) ,selected = 1
+    )
+  })
+  
+  output$endoscopistCol_documentqual<-renderUI({
+    selectInput("endoscopistColChooser_documentqual", label = h4("Choose the column showing the endoscopist:"),
+                choices = colnames(RV4$data) ,selected = 1
+    )
+  })
+  
+  output$endoscopistPick_documentqual<-renderUI({
+    selectInput("endoscopistPickChooser_documentqual", label = h4("Choose the endoscopist:"),
+                choices = unique(RV4$data[,input$endoscopistColChooser_documentqual]),selected = 1
+    )
+  })
+  
+  output$endoDoc_documentqual<-renderUI({
+    selectInput("endoDoc_documentqualChoose", label = h4("Choose the endoscopic documentation column"),
+                choices = colnames(RV4$data) ,selected = 1
+    )
+  })
+  
+  
+  
+  output$plotBarrQM <- renderPlot({
+    browser()
+    G = ggplot(RV4$data,  aes_string(x = input$WorstGradeChooser,fill=input$endoscopistPickChoose)) + 
+      geom_histogram(stat = "count")
+    G
+  })
+  
+  
+  myNotableWords <- c("[Ii]sland", "[Hh]iat|astric fold|[Pp]inch","esion|odule|lcer")
+  
+  
+  
+  output$plotBarrEQ <- renderPlot({
+    #browser()
+    
+    #Perform the lookup from EndoMiner for "[Ii]sland", Prague Score, "[Hh]iat|astric fold|[Pp]inch", "esion|odule|lcer"
+    jj <- ListLookup(RV4$data, input$endoDoc_documentqualChoose,myNotableWords)
+    ggplot(jj,  aes(x = X2,y=Prop)) + 
+      geom_bar(stat="identity")
+
+  })
+  
+
   
   ########### Polyp events ############     
   observeEvent(input$GRS,{
@@ -494,13 +627,12 @@ radiant.data::launch()
     } else if (input$data == "Barretts") {
       data_r$data <- RV4$data
       data_r$name <- "Barretts"
+    } else if (input$data == "Trimmed") {
+      data_r$data <- Trim$data
+      data_r$name <- "Trimmed"
     }
   },ignoreInit = TRUE)
   
-  
-
-
-    
   callModule(module = esquisserServer, id = "esquisse", data = data_r)
 
 }
