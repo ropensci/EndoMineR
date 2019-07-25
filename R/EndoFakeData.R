f7f416cb4c793e569bd056915fb50211effcc7eb
sebastiz <sebastiz@hotmail.com>
2019-07-25 17:45:22 +0000

    EndoMerge2 changed so it works without fuzzjoin

--- R/EndoFakeData.R
+++ R/EndoFakeData.R
@@ -28,35 +28,40 @@
 #' )
 Endomerge2 <-
   function(x,
-             EndoDate,
-             EndoHospNumber,
-             y,
-             PathDate,
-             PathHospNumber) {
+           EndoDate,
+           EndoHospNumber,
+           y,
+           PathDate,
+           PathHospNumber) {
+    
     # Rename the columns so can do the join Extract the date from both reports
     colnames(x)[which(names(x) == EndoDate)] <- "Date"
     colnames(x)[which(names(x) == EndoHospNumber)] <- "eHospitalNum"
     x$Date <- gsub("\n", "", x$Date)
     x$Date <- as.Date(x$Date)
-
+    
     colnames(x)[which(names(x) == EndoDate)] <- "Date"
     colnames(x)[which(names(x) == EndoHospNumber)] <- "eHospitalNum"
+    x$eHospitalNum <- trimws(gsub("\n", "", x$eHospitalNum))
+    
     colnames(y)[which(names(y) == PathDate)] <- "Date"
     colnames(y)[which(names(y) == PathHospNumber)] <- "pHospitalNum"
+    names(y)<-gsub("[.]","",names(y))
     y$Date <- gsub("\n", "", y$Date)
     y$Date <- as.Date(y$Date)
-
-    EndoHistoMerge <-
-      difference_full_join(y,
-        x,
-        by = "Date",
-        max_dist = 8,
-        distance_col = "Days"
-      )
-
-    EndoHistoMerge$pHospitalNum <- trimws(EndoHistoMerge$pHospitalNum)
-    EndoHistoMerge$eHospitalNum <- trimws(gsub("\n", "", EndoHistoMerge$eHospitalNum))
-    EndoHistoMerge1 <- EndoHistoMerge[EndoHistoMerge$eHospitalNum == EndoHistoMerge$pHospitalNum, ]
-
-    return(EndoHistoMerge1)
+    y$pHospitalNum <- trimws(y$pHospitalNum)
+    
+    
+    #Merge by hospital number first as a full join
+    EndoHistoMerge<-merge(x, y, by.x=c( "eHospitalNum"),
+                          by.y=c( "pHospitalNum"),
+                          all.x=TRUE)
+    
+    #Then create date diff
+    EndoHistoMerge$DayDiff<-difftime(EndoHistoMerge$Date.x, EndoHistoMerge$Date.y,units="days")
+    
+    #Then filter by datediff to get the final list including when no biopsies were taken
+    EndoHistoMerge<-EndoHistoMerge%>%filter((DayDiff<7&DayDiff>-1)|is.na(DayDiff))
+    
+    return(EndoHistoMerge)
   }
