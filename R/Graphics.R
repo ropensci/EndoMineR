dfab0bbcf35bcdc3051280b01e8059d22ab8e25c
sebastiz <sebastiz@hotmail.com>
2019-10-02 13:37:28 +0000

    changes

--- R/Graphics.R
+++ R/Graphics.R
@@ -108,8 +108,8 @@ theme_Publication <- function(base_size = 14, base_family = "Helvetica") {
 #' @family Data Presentation helpers
 #' @examples
 #' # None needed
-scale_fill_Publication <- function(...) {
-  discrete_scale("fill", "Publication", manual_pal(values = c("#386cb0", "#fdb462", "#7fc97f", "#ef3b2c", "#662506", "#a6cee3", "#fb9a99", "#984ea3", "#ffff33")), ...)
+scale_fill_Publication <- function() {
+  discrete_scale("fill", "Publication", manual_pal(values = c("#386cb0", "#fdb462", "#7fc97f", "#ef3b2c", "#662506", "#a6cee3", "#fb9a99", "#984ea3", "#ffff33")))
   
   }
 
@@ -128,8 +128,8 @@ scale_fill_Publication <- function(...) {
 #' @family Data Presentation helpers
 #' @examples
 #' # None needed
-scale_colour_Publication <- function(...) {
-  discrete_scale("colour", "Publication", manual_pal(values = c("#386cb0", "#fdb462", "#7fc97f", "#ef3b2c", "#662506", "#a6cee3", "#fb9a99", "#984ea3", "#ffff33")), ...)
+scale_colour_Publication <- function() {
+  discrete_scale("colour", "Publication", manual_pal(values = c("#386cb0", "#fdb462", "#7fc97f", "#ef3b2c", "#662506", "#a6cee3", "#fb9a99", "#984ea3", "#ffff33")))
 }
 
 
